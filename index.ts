149563a92f34765af955b2f4cb324c22a23fcc64
luna <luna@wvvw.me>
2025-03-12 18:40:04 +0000

    chore: init

---
+++ index.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+import { Bot } from '@skyware/bot';
+
+const username = process.env.BLUESKY_USERNAME;
+const password = process.env.BLUESKY_PASSWORD;
+
+if (!username || !password) throw new Error('BLUESKY_USERNAME and BLUESKY_PASSWORD must be set');
+
+const main = async () => {
+  const bot = new Bot();
+
+  await bot.login({
+    identifier: username,
+    password: password,
+  });
+
+  console.info(`bot logged in as ${username}`);
+
+  bot.on('reply', async (message) => {
+    if (!message.embed || !message.embed?.isImages()) {
+      console.info(`Message from ${message.author.displayName} is not an image: "${message.text}"`);
+      return;
+    }
+
+    const hasAltText = message.embed.images.every((image) => image.alt);
+    if (hasAltText) {
+      console.info(`Message from ${message.author.displayName} has alt text: "${message.text}"`);
+      return;
+    }
+
+    console.info(`Replying to message from ${message.author.displayName}: "${message.text}"`);
+    try {
+      await message.reply({
+        text: 'please add alt text to your images',
+      });
+    } catch (error) {
+      console.error(`Error replying to message from ${message.author.displayName}: "${message.text}"`, error);
+    }
+  });
+};
+
+main().catch(console.error);
