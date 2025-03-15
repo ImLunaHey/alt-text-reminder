2c50777d0de5dcef143d62bd0993c1688f15396a
luna <luna@wvvw.me>
2025-03-15 08:47:57 +0000

    fix: better error handling

--- index.ts
+++ index.ts
@@ -17,25 +17,30 @@ const main = async () => {
   console.info(`bot logged in as ${username}`);
 
   bot.on('reply', async (message) => {
-    if (!message.embed || !message.embed?.isImages()) {
-      console.info(`Message from ${message.author.displayName} is not an image.`);
-      return;
-    }
-
-    const hasAltText = message.embed.images.every((image) => image.alt);
-    if (hasAltText) {
-      console.info(`Message from ${message.author.displayName} has alt text.`);
-      return;
-    }
-
-    console.info(`Replying to message from ${message.author.displayName}.`);
     try {
+      if (!message.embed || !message.embed?.isImages()) {
+        console.info(`Message from ${message.author.displayName} is not an image.`);
+        return;
+      }
+
+      const hasAltText = message.embed.images.every((image) => image.alt);
+      if (hasAltText) {
+        console.info(`Message from ${message.author.displayName} has alt text.`);
+        return;
+      }
+
+      console.info(`Replying to message from ${message.author.displayName}.`);
+
       await message.reply({
         text: 'please add alt text to your images\n\nsettings → accessibility → require alt text before posting',
       });
     } catch (error) {
       console.error(`Error replying to message from ${message.author.displayName}.`, error);
     }
+  });
+
+  bot.on('error', (error) => {
+    console.error('Error:', error);
   });
 };
 
